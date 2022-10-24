f05ac6e53f2a53ac96069e2e666646c9ef09525b
menomik1011 <39692905+meno1011@users.noreply.github.com>
2022-10-24 18:06:58 +0900

    token 부분 변환

--- src/routes/api.js
+++ src/routes/api.js
@@ -8,12 +8,14 @@ admin.initializeApp({
 });
 
 route.post('/send', (req, res)=>{
-  const { title, text } = req.body;
-  let target_token = 
-  "dqpDiPHVj3FlnloRR5-KpM:APA91bEjHTzSp3VDafbNeEluoQYBT4yZlzUW9BFlbSosIuHGmylMoaKOjG8MSSIG7X_Kws0Z_iXs9rN0j3gjehSgp-OFtoilj4S_HYTdb8ZfaW0QVmxQfpgmpl_5UbN5vM2r8mO4C7UT"
+  const { title, text, token } = req.body;
+  // 직접 토큰 복붙 했던부분
+  // let target_token = 
+  // // "dqpDiPHVj3FlnloRR5-KpM:APA91bEjHTzSp3VDafbNeEluoQYBT4yZlzUW9BFlbSosIuHGmylMoaKOjG8MSSIG7X_Kws0Z_iXs9rN0j3gjehSgp-OFtoilj4S_HYTdb8ZfaW0QVmxQfpgmpl_5UbN5vM2r8mO4C7UT"
+  // "dG08ZiDeBmX-0OPnyapK1X:APA91bHXe-UNGWq6-wthmJs5UC6rz-mXH30avisUVphCNG6QzEWKpIAUVqajUw4-YmzdM_ZOPJNbTIm6pEyaVK6ZSC1TtlaEvggkanl2UWFf4MWZsAWNyh_UgP5GXlSGYdjKDs0Hil7w"
   admin.messaging()
     .sendToDevice(
-      [target_token],
+      [token],
       {
         data:{
           text: text
